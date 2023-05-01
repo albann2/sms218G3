7a7fd8b28f0b1aade56b533ff206bf7822d43715
kevinemalone <109515426+kevinemalone@users.noreply.github.com>
2023-05-01 17:04:37 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,42 +1,7 @@
 package com.sms;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.uimanager.IllegalViewOperationException; 
-
-import android.telephony.SmsManager;
-
-public class MySendSmsModule extends ReactContextBaseJavaModule {
- 
-    public MySendSmsModule(ReactApplicationContext reactContext) {
-        //required by React Native
-        super(reactContext);
-    }
- 
-    @Override
-    //getName is required to define the name of the module
-    public String getName() { 
-        return "DirectSms";
-    }
- 
-    @ReactMethod
-    public void sendDirectSms(String phoneNumber, String msg) {
-        try {  
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(
-              phoneNumber, null, msg, null, null
-            );
-        } catch (Exception ex) {
-            System.out.println("couldn't send message.");
-        } 
-    }
-}
 
 
-
-
-/*import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
@@ -46,7 +11,7 @@ public class MainActivity extends ReactActivity {
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
-   
+   */
   @Override
   protected String getMainComponentName() {
     return "SMS";
@@ -56,7 +21,7 @@ public class MainActivity extends ReactActivity {
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
    * (aka React 18) with two boolean flags.
-   
+   */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new DefaultReactActivityDelegate(
@@ -68,4 +33,4 @@ public class MainActivity extends ReactActivity {
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
   }
-}*/
+}
